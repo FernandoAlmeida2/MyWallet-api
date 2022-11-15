4df33658f300456de36ac93563962e20a98ce7bc
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-11-15 17:24:20 -0300

    create route to register an expense or income

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import Joi from "joi";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
 
 //Schemas
 const userSchema = Joi.object({
@@ -17,6 +18,12 @@ const userSchema = Joi.object({
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]*$")).required(),
+});
+
+const historySchema = Joi.object({
+  amount: Joi.number().min(0).required(),
+  description: Joi.string().min(12).required(),
+  type: Joi.string().valid("income", "expense").required(),
 });
 
 //configs
@@ -70,14 +77,49 @@ server.post("/sign-in", async (req, res) => {
     const user = await db.collection("users").findOne({ email });
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = uuidv4();
+      console.log(token)
       await db.collection("sessions").insertOne({
         userId: user._id,
         token,
       });
-      res.status(202).send({message: "Login was successful!", token})
+      res.status(202).send({ message: "Login was successful!", token });
     } else {
       return res.status(404).send({ message: "email/password invalid!" });
     }
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+server.post("/history", async (req, res) => {
+  const validation = historySchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    const errorsList = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errorsList);
+  }
+
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  if (!token) {
+    return res
+      .status(401)
+      .send({ message: "token is required! (format: Bearer token)" });
+  }
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) {
+      return res.status(401).send({ message: "Invalid token!" });
+    }
+    /* const user = await db.collection("users").findOne({ _id: session.userId });
+    if (!user) {
+      return res.status(401).send({ message: "User not found!" });
+    } */
+    await db.collection("history").insertOne({
+      ...req.body,
+      userId: session.userId,
+      date: dayjs().format("DD/MM"),
+    });
+    res.status(201).send({ message: "Your history has been updated!" });
   } catch (err) {
     res.sendStatus(500);
   }
